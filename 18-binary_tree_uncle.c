904c6d19c0945c8d234ec25f7d24c0ff7c55bfb3
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-10-03 20:09:03 +0300

    Binary 18 - 01

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,18 +10,18 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL)
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 	{
 		return (NULL);
 	}
 
-	if (node->parent->left == node)
+	if (node->parent->parent->left == node->parent)
 	{
-		return (node->parent->right);
+		return (node->parent->parent->right);
 	}
 
 	else
 	{
-		return (node->parent->left);
+		return (node->parent->parent->left);
 	}
 }
