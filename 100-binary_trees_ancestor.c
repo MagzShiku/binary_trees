98f2561b7c39dc5bc07a7bd5a94494887fb743e6
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-10-04 10:38:18 +0300

    Task 19 - 00

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,14 +1,36 @@
 #include "binary_trees.h"
 
 /**
- * binary_trees_ancestor - finds the uncle of a node
- * @node: pointer to the node to find the uncle
- * Return: return a pointer to the uncle node
- * If node is NULL, return NULL
- * If node has no uncle, return NULL
+ * binary_trees_ancestor - lowest common ancestor of two nodes
+ * @first: pointer to the first node
+ * @second: pointer to the second node
+ * Return: pointer to the lowest common ancestor node
+ * If no common ancestor was found, your function must return NULL
  */
 
-binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
-	
+	binary_tree_t *ancestor;
+	binary_tree_t *temp;
+
+	if (first == NULL || second == NULL)
+		return (NULL);
+
+	ancestor = (binary_tree_t *)first;
+
+	while (ancestor)
+	{
+		temp = (binary_tree_t *)second;
+
+		while (temp)
+		{
+			if (ancestor == temp)
+				return (ancestor);
+
+			temp = temp->parent;
+		}
+		ancestor = ancestor->parent;
+	}
+	return (NULL);
 }
